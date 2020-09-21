20c15ed51ab42f188307dab591fe9b9150714445
Jonathan Dursi <jonathan@dursi.ca>
2020-09-21 10:31:23 -0400

    Fix sample reference

--- insert_child.py
+++ insert_child.py
@@ -147,7 +147,7 @@ def ingest(connection, cursor, data, samples, vcffile, dataset_name, dataset_des
             variant_id = cursor.fetchone()[0]
             cur_var_id = cur_var_id + 1
 
-            samples_w_variant = [(variant_id, sample_to_id[samp + "_" + access_type]) for samp in has_var if samp in sample_to_id]
+            samples_w_variant = [(variant_id, sample_to_id[samp + "_" + access_type]) for samp in has_var if samp + "_" + access_type in sample_to_id]
             cursor.executemany("""INSERT INTO variant_sample_table(variant_id, sample_id)
                                   VALUES(%s,%s)""", samples_w_variant)
             count += 1
